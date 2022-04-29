9fcdf3860bafca180111fabecf89a725471e0afe
zhaoxinnZ <zhaoxinzhang0429@gmail.com>
2022-04-29 11:20:50 +0800

    fixed

--- src/com/fju/order/Order.java
+++ src/com/fju/order/Order.java
@@ -1,11 +1,23 @@
 package com.fju.order;
 
 public class Order {
+    int shipFeed;
     int amount;
     Delivery delivery;
 
     public Order(int amount, Delivery delivery) {
+
         this.amount = amount;
         this.delivery = delivery;
+        if (delivery instanceof ShopeeDelivery && amount > 199){
+            this.shipFeed = 0;
+        }else {
+            this.shipFeed = delivery.price;
+        }
     }
+
+    public int total() {
+        return amount + shipFeed;
+    }
+
 }
--- src/com/fju/order/Seven11Delivery.java
+++ src/com/fju/order/Seven11Delivery.java
@@ -4,5 +4,7 @@ public class Seven11Delivery extends Delivery {
     public Seven11Delivery() {
         name = "7-11";
         price = 60;
+
+
     }
 }
